6e9927bf422d247f179d245a9b4b16813bdaf90c
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-12-05 16:29:35 +0300

    Add R6 monster

--- scratch.R
+++ scratch.R
@@ -1,3 +1,6 @@
+library(rlang)
+library(R6)
+library(collections)
 library(tidyverse)
 
 # right now it is quite messy
@@ -14,7 +17,11 @@ losing_p_distr = c(5, 2, 2, 1, .1, .03) |>
   scale_p_distr()
 
 price = 10
+cash = 10000
 n_bandits = 20 # constant
+
+n_steps = 1000
+n_iters = 100
 
 winning_sizes = c(0, price %/% 2, price, price*2, price*10, price*100)
 
@@ -46,6 +53,91 @@ sigmoid = function(x, c1, c2) {
 }
 
 sigmoid_paramed = \(x) sigmoid(x, c1=.001, c2=5000)
+
+
+GameEnvironment = R6Class("GameEnvironment",
+  public = list(
+    # attributes from player
+    n = 1,
+    rewards = NULL,
+    indices = NULL,
+
+    # attributes for casino
+    p_distrs = NULL,
+    winner = NULL,
+    changes = NULL, # на каком шаге какой автомат был выигрышным
+
+    history_prizes = list(),
+    last_change = 0,
+
+    initialize = function(n_steps) {
+      self$rewards = vector('list', n_steps)
+      self$indices = vector('list', n_steps)
+
+      self$winner = get_winning_bandit()
+      self$p_distrs = get_distrs(self$winner)
+
+      self$changes = list(c(self$winner, 1))
+      self$history_prizes <- vector('numeric', length = n_bandits)
+
+      },
+
+    pull_machine = function(index) {
+      reward = pull_bandit(self$p_distrs[[index]])
+
+      self$indices[[self$n]] = index
+      self$rewards[[self$n]] = reward
+      self$n = self$n + 1
+
+      private$update_machines()
+      self$history_prizes[[index]] = self$history_prizes[[index]] +
+        reward
+
+      reward
+    }
+  )
+  , private = list(
+    update_machines = function() {
+      self$last_change = self$last_change + 1
+
+      if ( # если автомат больше всего приносит выгоды и >= 200 игр
+          (self$winner == which.max(self$history_prizes)) &
+          (self$last_change >= 200)
+          ) {
+        # значение на сигмоиде -> 1, чем больше прибыли от автомата
+        p=sigmoid_paramed(self$history_prizes[[self$winner]])
+        print(p)
+        is_change=sample(0:1, size=1, prob=c(1-p, p))
+
+        if (is_change) {
+          self$winner <- get_winning_bandit(self$winner)
+          self$p_distrs <- get_distrs(self$winner)
+          self$history_prizes <- vector('numeric', length = n_bandits)
+          self$changes = append(
+            self$changes, list(c(self$winner, self$n))
+            )
+          self$last_change = 0
+          }
+        }
+      }
+    )
+)
+
+g1 = GameEnvironment$new(n_steps)
+# g1$indices
+g1$pull_machine(20)
+g1$last_change
+g1$n
+
+g1$history_prizes
+
+g1$changes
+g1$rewards[1:20]
+
+g1$winner
+g1$history_prizes
+
+
 
 iter_play = 0
 change_p_distrs = function(history_prizes, winner) {
@@ -156,9 +248,10 @@ strategy1 = function() {
       1:n_bandits, size=1, prob=probs_bandit_to_play
     )
     result = pull_bandit_player(index_choice)
+    # update vars in closure
     results[[index_choice]] <<- results[[index_choice]] + result
   }
-  results
+  make_step
 }
 
 run_strategy_one_iter = function(strategy_func) {
@@ -188,3 +281,136 @@ for (iter in 1:100){
 
 # visualization -------------------------------------------------------
 
+
+# demo structure ------------------------------------------------------
+
+iter_play = 0
+change_p_distrs = function(history_prizes, winner) {
+  # print('hello')
+  .GlobalEnv$iter_play = .GlobalEnv$iter_play + 1
+  if (winner == which.max(history_prizes)) {
+    # print('max winner condition')
+    p=sigmoid_paramed(history_prizes[[winner]])
+    is_change=sample(0:1, size=1, prob=c(1-p, p))
+    # print(is_change)
+
+    if (is_change) {
+      winner <- get_winning_bandit(winner)
+      .GlobalEnv$winner = winner
+      p_distrs <- get_distrs(winner)
+      .GlobalEnv$p_distrs = p_distrs
+      history_prizes <- vector('numeric', length = n_bandits)
+      .GlobalEnv$history_prizes = history_prizes
+      .GlobalEnv$changes = append(
+        .GlobalEnv$changes, list(c(winner, iter_play))
+        )
+    }
+  }
+  # list(p_distrs)
+}
+
+GameEnvironment = R6Class("GameEnvironment",
+  public = list(
+    # attributes from player
+    n = 1,
+    rewards = list(),
+    indices = list(),
+
+    # attributes for casino
+    p_distrs = NULL,
+    winner = NULL,
+    changes = NULL, # на каком шаге какой автомат был выигрышным
+
+    history_prizes = list(),
+    last_change = 0,
+
+    initialize = function(n_steps) {
+      self$b <- b
+      self$rewards = vector('list', n_steps)
+      self$indices = vector('list', n_steps)
+
+      self$winner = get_winning_bandit()
+      self$p_distrs = get_distrs(self$winner)
+
+      self$changes = list(c(self$winner, 1))
+
+      },
+
+    pull_machine = function(index) {
+      reward = pull_bandit_player(index)
+
+      self$indices[[n]] = index
+      self$rewards[[n]] = reward
+      self$n = self$n + 1
+
+      self$update_machines()
+
+      reward
+    }
+  )
+  , private = list(
+    update_machines = function() {
+      self$last_change = self$last_change + 1
+
+      if ( # если автомат больше всего приносит выгоды и >= 200 игр
+          (self$winner == which.max(self$history_prizes)) &
+          (self$last_change >= 200)
+          ) {
+        # значение на сигмоиде -> 1, чем больше прибыли от автомата
+        p=sigmoid_paramed(self$history_prizes[[self$winner]])
+        is_change=sample(0:1, size=1, prob=c(1-p, p))
+
+        if (is_change) {
+          self$winner <- get_winning_bandit(winner)
+          self$p_distrs <- get_distrs(winner)
+          self$history_prizes <- vector('numeric', length = n_bandits)
+          self$changes = append(
+            self$changes, list(c(self$winner, self$n))
+            )
+          self$last_change = 0
+          }
+        }
+      }
+    )
+)
+c1 = capture_output$new(3)
+c1$rewards()
+
+c1$f()
+c1$a
+
+
+
+strategy_demo = function(play_tracker) {
+  pull = play_tracker()
+  moving_avg = c()
+  make_step = function() {
+
+  }
+
+  make_step
+}
+
+f1 = function() {
+  e1 = env(
+    a=0
+  )
+  i1 = function() {
+    e1$a = e1$a + 1
+    e1$a
+  }
+  i1
+}
+
+
+ff1 = f1()
+ff1()
+
+ff2 = f1()
+ff2()
+
+
+
+
+y <- 1
+f <- function(x) x + y
