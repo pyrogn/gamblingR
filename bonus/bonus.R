dea87c18bc7941dd32a6b82dcab7b6a9fe7f275c
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-12-15 10:22:06 +0300

    Two more functions

--- bonus/bonus.R
+++ bonus/bonus.R
@@ -18,7 +18,7 @@ top <- splitted_tree[is_top]
 trunk <- splitted_tree[!is_top] |>
   map_chr(~ brown(.) |> str_conv("utf-8"))
 
-
+# определение порядкового номера ветки для градиента
 cnt_stars <- map_int(top, \(x) str_count(x, "\\*"))
 cnt_stars_shifed <- cnt_stars[2:length(cnt_stars)]
 cnt_diff <- cnt_stars - cnt_stars_shifed
@@ -70,7 +70,7 @@ split_level <- function(row) {
   n <- nchar(row)
   n_stars <- n - n_spaces
   spaces <- rep(" ", n_spaces) |> str_flatten()
-  return(list(space = n_spaces, star = n_stars))
+  return(list(n_spaces = n_spaces, n_stars = n_stars))
 }
 # цвета гирлянд
 colors <- c(
@@ -79,38 +79,43 @@ colors <- c(
   col_br_white,
   col_br_blue
 )
+random_n <- function(n) {
+  sample(1:n, size = 1)
+}
+rep_concat <- function(sym, n) {
+  rep(sym, n) |>
+    str_flatten()
+}
 # обработка одной ветки
 process_row <- function(row, ind) {
   splitted_n <- split_level(row)
   style <- branch2ansi_style(ind)
-  if (sample(1:2, size = 1) == 1) {
-    position <- sample(1:splitted_n$star, size = 1)
-    color <- colors[[sample(1:length(colors), size = 1)]]
+  if (random_n(2) == 1) { # 50% chance
+    position <- random_n(splitted_n$n_stars)
+    color <- colors[[random_n(length(colors))]]
     stars <- place_light(
-      splitted_n$star,
+      splitted_n$n_stars,
       style,
       position,
       color("O")
     )
   } else {
-    stars <- rep("*", splitted_n$star) |>
-      str_flatten() |>
+    stars <- rep_concat("*", splitted_n$n_stars) |>
       style()
   }
-  spaces <- rep(" ", splitted_n$space) |> str_flatten()
+  spaces <- rep_concat(" ", splitted_n$n_space)
   c(spaces, stars) |> str_flatten()
 }
 
 for (i in 1:3000) {
   # с глаз долой прошлый кадр
-  newlines <- rep("\n", 5) |> str_flatten()
-  top_mod <- imap_chr(top, process_row)
-  c_res <- c(top_mod, trunk)
-  final_res <- c_res |>
-    str_c("\n") |>
-    str_flatten()
+  newlines <- rep_concat("\n", 8)
 
-  to_cat <- c(newlines, final_res) |> str_flatten()
+  top_mod <- imap_chr(top, process_row)
+  tree_colored <- c(top_mod, trunk) |>
+    str_flatten(collapse = "\n")
+
+  to_cat <- c(newlines, tree_colored) |> str_flatten()
 
   # system('clear') # might flicker
   cat(to_cat)
